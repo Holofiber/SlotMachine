a965081dcf9d60a5d97f7087f510c73da7ea0898
Holofiber <aminus2010@gamil.com>
2017-10-30 20:22:42 +0200

    SM: Add Slot machine project

--- SlotMachine/Form1.Designer.cs
+++ SlotMachine/Form1.Designer.cs
@@ -28,12 +28,144 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.slot1 = new System.Windows.Forms.PictureBox();
+            this.slot3 = new System.Windows.Forms.PictureBox();
+            this.slot2 = new System.Windows.Forms.PictureBox();
+            this.credit = new System.Windows.Forms.Label();
+            this.win = new System.Windows.Forms.Label();
+            this.bet = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.slot1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.slot3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.slot2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // slot1
+            // 
+            this.slot1.Location = new System.Drawing.Point(158, 99);
+            this.slot1.Name = "slot1";
+            this.slot1.Size = new System.Drawing.Size(150, 150);
+            this.slot1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.slot1.TabIndex = 0;
+            this.slot1.TabStop = false;
+            // 
+            // slot3
+            // 
+            this.slot3.Location = new System.Drawing.Point(470, 99);
+            this.slot3.Name = "slot3";
+            this.slot3.Size = new System.Drawing.Size(150, 150);
+            this.slot3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.slot3.TabIndex = 1;
+            this.slot3.TabStop = false;
+            // 
+            // slot2
+            // 
+            this.slot2.Location = new System.Drawing.Point(314, 99);
+            this.slot2.Name = "slot2";
+            this.slot2.Size = new System.Drawing.Size(150, 150);
+            this.slot2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.slot2.TabIndex = 2;
+            this.slot2.TabStop = false;
+            // 
+            // credit
+            // 
+            this.credit.AutoSize = true;
+            this.credit.Location = new System.Drawing.Point(155, 71);
+            this.credit.Name = "credit";
+            this.credit.Size = new System.Drawing.Size(37, 13);
+            this.credit.TabIndex = 3;
+            this.credit.Text = "Credit:";
+            // 
+            // win
+            // 
+            this.win.AutoSize = true;
+            this.win.Location = new System.Drawing.Point(467, 71);
+            this.win.Name = "win";
+            this.win.Size = new System.Drawing.Size(29, 13);
+            this.win.TabIndex = 4;
+            this.win.Text = "Win:";
+            // 
+            // bet
+            // 
+            this.bet.AutoSize = true;
+            this.bet.Location = new System.Drawing.Point(311, 71);
+            this.bet.Name = "bet";
+            this.bet.Size = new System.Drawing.Size(26, 13);
+            this.bet.TabIndex = 5;
+            this.bet.Text = "Bet:";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(198, 64);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(33, 20);
+            this.textBox1.TabIndex = 6;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(343, 64);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(33, 20);
+            this.textBox2.TabIndex = 7;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(502, 64);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(33, 20);
+            this.textBox3.TabIndex = 8;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(158, 255);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(462, 23);
+            this.button1.TabIndex = 9;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1019, 471);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.bet);
+            this.Controls.Add(this.win);
+            this.Controls.Add(this.credit);
+            this.Controls.Add(this.slot2);
+            this.Controls.Add(this.slot3);
+            this.Controls.Add(this.slot1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.slot1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.slot3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.slot2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.PictureBox slot1;
+        private System.Windows.Forms.PictureBox slot3;
+        private System.Windows.Forms.PictureBox slot2;
+        private System.Windows.Forms.Label credit;
+        private System.Windows.Forms.Label win;
+        private System.Windows.Forms.Label bet;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Button button1;
     }
 }
 
--- SlotMachine/Form1.cs
+++ SlotMachine/Form1.cs
@@ -4,6 +4,7 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -15,6 +16,87 @@ namespace SlotMachine
         public Form1()
         {
             InitializeComponent();
+        }
+
+        public static long credits = 100;
+        public static long total = 0;
+        public static int bets = 5;
+
+        public static int p1;
+        public static int p2;
+        public static int p3;
+
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            slot1.Image = Image.FromFile("1.png");
+            slot2.Image = Image.FromFile("2.png");
+            slot3.Image = Image.FromFile("3.png");
+        }
+
+        public static class IntUtil
+        {
+            private static Random random;
+
+            private static void Init()
+            {
+                if (random == null) random = new Random();
+            }
+
+            public static int Random(int min, int max)
+            {
+                Init();
+                return random.Next(min, max);
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (credits >= bets)
+            {
+                credits = credits - bets;
+                textBox1.Text = credits.ToString();
+
+                for (var i = 0; i < 10; i++)
+                {
+                    p1 = IntUtil.Random(1, 4);
+                    p2 = IntUtil.Random(1, 4);
+                    p3 = IntUtil.Random(1, 4);
+
+                }
+
+                if (slot1.Image != null)
+                {
+                    slot1.Image.Dispose();
+                    slot1.Image = Image.FromFile(p1.ToString() + ".png");
+                }
+                if (slot2.Image != null)
+                {
+                    slot2.Image.Dispose();
+                    slot2.Image = Image.FromFile(p2.ToString() + ".png");
+                }
+                if (slot3.Image != null)
+                {
+                    slot3.Image.Dispose();
+                    slot3.Image = Image.FromFile(p3.ToString() + ".png");
+                }
+
+                total = 0;
+
+                if (p1 == 3) total = total + 5;
+                if (p1 == 2 & p2 == 2) total = total + 10;
+                if (p1 == 3 & p2 == 3) total = total + 10;
+                if (p1 == 3 & p2 == 3 & p3 ==3) total = total + 20;
+                if (p1 == 2 & p2 == 2 & p3 ==2) total = total + 30;
+                if (p1 == 1 & p2 == 1 & p3 ==1) total = total + 50;
+
+                credits = credits + total;
+                textBox3.Text = total.ToString();
+                textBox1.Text = credits.ToString();
+
+
+            }
         }
     }
 }
